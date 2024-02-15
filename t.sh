4001ee69d141550206255626b40ba9e12e5471c2
Allen Guo <guosonglin001@gmail.com>
2024-02-15 15:47:59 +0000

    wip

--- t.sh
+++ t.sh
@@ -6,9 +6,46 @@ docker run -d -it \
 
 # #############################################################################
 
+cat >>~/.bashrc <<-EOF
+export PATH=\$PATH:\${EXT_PATH}
+export PYTHONPATH=\$PYTHONPATH:\${EXT_PYTHONPATH}
+EOF
+
+apt update
+apt install -y locales
+apt install -y bison
+apt install -y flex
+apt install -y gperf
+micromamba install bear abseil-cpp
+
+# for locales warn
+sudo locale-gen en_US en_US.UTF-8
+sudo dpkg-reconfigure locales
+
+# start build
+autoconf
+
+# just configure, do not build
+CC=clang \
+  CXX=clang++ \
+  ./configure --prefix=$PWD/build/install_2
+
+# build with cmake target `install`
+
+cp include/verilated.mk build/install/include/
+cp bin/verilator_includer build/install/bin/
+
+# #############################################################################
+
 verilator --help
 
-build/src/verilator_bin_dbg \
-  -o hello examples/hello.vl
+pushd examples/make_hello_binary
+make
+./obj_dir/Vtop
+popd
 
-verilated.mk
+pushd examples/cmake_hello_c
+cmake .
+cmake --build .
+./example
+popd
